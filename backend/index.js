2b9726ac25160c94f5e2c4759591970b53cc14f4
Subhashis <subhashisdhara123@gmail.com>
2025-07-22 00:36:34 +0530

    Update CORS configuration to allow requests from the production URL

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ dotenv.config();
 
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://vybe-4jbw.onrender.com"],
   credentials: true,
 }))
 app.use(express.json());
--- backend/socket.js
+++ backend/socket.js
@@ -7,12 +7,12 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://vybe-4jbw.onrender.com"],
     methods: ["GET", "POST"],
   },
 });
 
-const useSocketMap={} 
+const useSocketMap = {};
 
 export const getSocketId = (receiverId) => {
   return useSocketMap[receiverId];
