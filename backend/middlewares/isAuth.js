a0c1107f50644586fabb1bdf3417f6a8eac8280a
Subhashis <subhashisdhara123@gmail.com>
2025-07-22 01:10:59 +0530

    Refactor isAuth middleware to improve token handling and ensure valid user ID assignment

--- backend/middlewares/isAuth.js
+++ backend/middlewares/isAuth.js
@@ -1,19 +1,12 @@
-
-import jwt from "jsonwebtoken";
-
-const isAuth = (req, res, next) => {
+const auth = (req, res, next) => {
   const token = req.cookies.token;
-  if (!token) {
-    return res.status(401).json({ message: "Unauthorized access" });
-  }
+  if (!token) return res.status(401).json({ message: "Unauthorized access" });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = decoded._id;
+    req.userId = decoded.id; 
     next();
   } catch (error) {
     return res.status(401).json({ message: "Invalid token" });
   }
 };
-
-export default isAuth;
